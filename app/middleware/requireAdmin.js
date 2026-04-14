ebec4925690bfe2010ba8b5467911e96b0d1dcaf
Vatsal17 <tailorvatsal17@gmail.com>
2026-04-14 17:08:38 +0100

    Delete app directory

--- app/middleware/requireAdmin.js
+++
@@ -1,9 +0,0 @@
-// Middleware to require admin role
-module.exports = (req, res, next) => {
-  if (!req.role || req.role !== 'admin') {
-    return res.status(403).send({
-      message: "Access denied. Only admins can access this resource."
-    });
-  }
-  next();
-};
--- app/middleware/requirePartner.js
+++
@@ -1,9 +0,0 @@
-// Middleware to require partner role
-module.exports = (req, res, next) => {
-  if (!req.role || req.role !== 'partner') {
-    return res.status(403).send({
-      message: "Access denied. Only partners can access this resource."
-    });
-  }
-  next();
-};
